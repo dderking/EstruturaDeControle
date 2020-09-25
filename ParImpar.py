cc85d413c960110fbfda87d65a6a87eb681e788e
Vini <62653539+ViniciusMedeiros96@users.noreply.github.com>
2020-09-25 17:32:57 -0300

    Create ParImpar.py

---
+++ ParImpar.py
@@ -0,0 +1,16 @@
+//
+N = int(input())
+for i in range(N):
+    x = int(input())
+    if x == 0:
+        print("NULL")
+    elif x > 0:
+        if x % 2 == 0:
+            print("EVEN POSITIVE")
+        else:
+            print("ODD POSITIVE")
+    elif x < 0:
+        if x % 2 == 0:
+            print("EVEN NEGATIVE")
+        else:
+            print("ODD NEGATIVE")
